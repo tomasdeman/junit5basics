27bc5f687a152822d59bc0ed6993f748a343c18a
tomasdeman <tomas.deman@gmail.com>
2019-06-10 22:52:22 +0200

    assertThrows

--- src/test/java/be/tomasdeman/MathUtilsTest.java
+++ src/test/java/be/tomasdeman/MathUtilsTest.java
@@ -15,6 +15,13 @@ class MathUtilsTest extends Object {
     }
 
     @Test
+    void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide by zero should throw");
+    }
+
+    @Test
     void testComputeCircleArea() {
         MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area.");
