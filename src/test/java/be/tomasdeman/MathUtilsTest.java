6ad62ab3a013c73daa9d74eac272cf7df4e28958
tomasdeman <tomas.deman@gmail.com>
2019-06-11 20:01:14 +0200

    @Nested inner class

--- src/test/java/be/tomasdeman/MathUtilsTest.java
+++ src/test/java/be/tomasdeman/MathUtilsTest.java
@@ -1,29 +1,52 @@
 package be.tomasdeman;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 class MathUtilsTest extends Object {
 
-    @Test
-    void testAdd() {
-        MathUtils mathUtils = new MathUtils();
-        int expected = 2;
-        int actual = mathUtils.add(1, 1);
-        assertEquals(expected, actual, "The add function should add two numbers");
+
+    MathUtils mathUtils;
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
+    @Nested
+    class AddTest {
+
+        @Test
+        @DisplayName("Testing add method for +")
+        void testAddPositive() {
+            assertEquals(2, mathUtils.add(1, 1), "The add function should add two numbers");
+        }
+
+        @Test
+        @DisplayName("Testing add method for -")
+        void testAddNegative() {
+            assertEquals(-2, mathUtils.add(-1, -1), "The add function should add two numbers");
+        }
     }
 
     @Test
     void testDivide() {
-        MathUtils mathUtils = new MathUtils();
-
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide by zero should throw");
     }
 
     @Test
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2, 2)),
+                () -> assertEquals(0, mathUtils.multiply(2, 0)),
+                () -> assertEquals(-2, mathUtils.multiply(2, -1))
+        );
+    }
+
+    @Test
     void testComputeCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area.");
     }
 
